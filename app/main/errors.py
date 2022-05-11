835aadd973045a2c21e29108c5afb0c6fb794227
mccafee025 <ademaamanka@gmail.com>
2022-05-11 13:13:01 +0300

    add changes to files

--- app/main/errors.py
+++ app/main/errors.py
@@ -2,4 +2,5 @@ from flask import render_template
 from . import main
 @main.app_errorhandler(404)
 def four_Ow_four(error):
+    title = '404 ERROR'
     return render_template('fourOwfour.html'),404
--- app/models.py
+++ app/models.py
@@ -2,18 +2,25 @@ from enum import unique
 from operator import index
 from . import db
 from flask_login import UserMixin, current_user
+from sqlalchemy.orm import backref
 from . import login_manager
 from werkzeug.security import generate_password_hash,check_password_hash
 from datetime import datetime, time
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
 class User(UserMixin,db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(32),unique=True,nullable = False)
     email = db.Column(db.String(50),unique=True,nullable = False)
     pass_secure = db.Column(db.String(32),nullable = False)
-    profile_pic_path = db.Column(db.String(50))
-    def __repr__(self):
-        return f'User {self.username}'
+    profile_pic_path = db.Column(db.String())
+    bio = db.Column(db.String(255))
+    post = db.relationship('Pitch', backref = 'user', lazy = 'dynamic')
+    comment = db.relationship('Comment', backref = 'user', lazy = 'dynamic')
+    upvote = db.relationship ('upvote', backref = 'user', lazy = 'dynamic')
+    downvote = db.relationship('Downvote', backref = 'user', lazy = 'dynamic')
     @property
     def password(self):
         raise AttributeError('You cannot access the password attribute')
@@ -22,6 +29,14 @@ class User(UserMixin,db.Model):
         self.pass_secure = generate_password_hash(password)
     def verify_password(self,password):
         return check_password_hash(self.pass_secure,password)
+    def save_user(self):
+        db.session.add(self)
+        db.session.commit()
+    def delete_user(self):
+        db.session.delete(self)
+        db.session.commit()
+    def __repr_(self):
+        return f'User {self.username}'
 class Pitch(db.Model):
     __tablename__ = 'pitches'
     id = db.Column(db.Integer, primary_key = True)
@@ -29,12 +44,64 @@ class Pitch(db.Model):
     pitch = db.Column(db.Text(), nullable = False)
     time_posted = db.Column(db.DateTime, default = datetime.utcnow)
     category_of_the_pitch = db.Column(db.String(150), index = True, nullable = False)
+    @classmethod
+    def fetch_pitches(cls,id):
+        pitches = Pitch.query.order_by(post_id= id).desc().all()
+        return pitches
     def save_pitch(self):
         db.session.add(self)
+        db.session.commit()
+    def delete_pitch(self):
+        db.session.delete(self)
         db.session.commit()
     def __repr__(self):
         return f'Pitch {self.pitch}'
 class Comment(db.Model):
     __tablename__ = 'comment'
     id = db.Column(db.Integer, primary_key = True)
+    comment = db.Column(db.Text(), nullable = False)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'),nullable = False)
+    post_id = db.Column(db.Integer,db.ForeignKey('pitches.id'),nullable = False)
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def fetch_comments(cls,id):
+        comments = Comment.query.filter_by(post_id=id).all()
+        return comments
+    def __repr__(self):
+        return f'comment:{self.comment}'
+class Upvote(db.Model):
+    __tablename__ = 'upvotes'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    def save_upvotes(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def fetch_by_post(cls,id):
+        upvote_by_post=Upvote.query.filter_by(post_id=id).all()
+        return upvote_by_post
+    def __repr__(self) -> str:
+        return f'Upvote{self.user_id}:{self.post_id}'
+class Downvote(db.Model):
+    __tablename__ = 'downvotes'
+    id = db.Column(db.Integer, primary_key = True)
+    user_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+    post_id = db.Column(db.Integer,db.ForeignKey('pitches.id'))
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+    @classmethod
+    def fetch_downvotes(cls,id):
+        downvote = Downvote.query.filter_by(post_id = id).all()
+        return downvote
+    def __repr__(self) -> str:
+        return f'{self.user_id}:{self.post_id}'
+
+
+
+
+
 
