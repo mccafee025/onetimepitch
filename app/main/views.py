bb705bb3e6b6ad81e9e4e9a89f8fd8b1435705f0
mccafee025 <ademaamanka@gmail.com>
2022-05-11 13:52:59 +0300

    add template for login page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,5 @@
 from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
-from . import auth
 from ..models import User, Pitch, Comment, Upvote, Downvote
 from .. import db,photos
 from flask_login import login_required, current_user
