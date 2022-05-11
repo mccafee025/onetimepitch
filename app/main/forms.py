7e91f9d030e43f3ec1e14b8acebdee0092502751
mccafee025 <ademaamanka@gmail.com>
2022-05-11 13:25:42 +0300

    add a form for pitches

---
+++ app/main/forms.py
@@ -0,0 +1,15 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, SelectField, TextAreaField, SubmitField, ValidationError
+from wtforms.validators import Required,Email,EqualTo
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about yourself.',validators=[Required()])
+    submit = SubmitField('Submit')
+
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Job','Job'),('Advertisement','Advertisement')],validators=[Required()])
+    post = TextAreaField('write your pitch here...', validators=[Required()])
+    submit = SubmitField('Post')
+
+
